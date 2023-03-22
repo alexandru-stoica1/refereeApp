8cc86133d2fd32955dff5b67ac0d2d3a3804759a
alexandru-stoica1 <stoica_alexandru_patru@yahoo.com>
2023-03-22 17:45:55 +0200

    -

---
+++ app/src/main/java/com/example/referee/QuizzActivity.java
@@ -0,0 +1,14 @@
+package com.example.referee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuizzActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quizz);
+    }
+}
